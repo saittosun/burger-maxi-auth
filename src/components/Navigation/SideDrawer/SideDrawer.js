f316db29e16aa91a42969e0e88a515a20f291e90
tostos <saittosun2@gmail.com>
2020-04-23 07:18:57 +0200

    adding a logout link

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -23,7 +23,7 @@ const sideDrawer = (props) => {//return something will not be the jsx code immed
           <Logo  />
         </div>
         <nav>
-          <NavigationItems />
+          <NavigationItems isAuthenticated={props.isAuth} />
         </nav>
       </div>
     </Aux>
